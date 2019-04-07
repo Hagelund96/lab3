6a082804b0f9698c0a22b7a625a35b6de6a210f3
Odd Arne Hagelund <oddahag@stud.ntnu.no>
2019-04-07 21:38:09 +0200

    repo

---
+++ app/src/main/java/oddahag/stud/ntnu/no/lab3/MainActivity.java
@@ -0,0 +1,162 @@
+package oddahag.stud.ntnu.no.lab3;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.function.DoubleToIntFunction;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    public static final int RESULT_CODE = 0;
+    private static int MIN_ACC;
+    private double first = 0;
+    private double second = 0;
+    private double third = 0;
+    private double score;
+    private double sec;
+    ArrayList<Double> al = new ArrayList<Double>();
+
+    private TextView p1, p2, p3;
+
+    // What to do when the sensor catches movement
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView yourScore;
+        yourScore = findViewById(R.id.yourScore);
+
+        if (event.values[0] >= MIN_ACC || event.values[1] >= MIN_ACC || event.values[2] >= MIN_ACC){
+            // start game
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            double ACC = calculateACC(x, y, z).doubleValue();
+            al.add(ACC);
+
+            if (al.size() < 20) {
+                onSensorChanged(event);
+            }
+        }
+
+        Collections.sort(al);
+        double temp = al.get(1);
+
+        score = (temp*temp)/(9.8*2);
+
+        sec = (2*temp*1)/9.8;
+        Double ms = sec * 1000;
+        try {
+            Thread.sleep(ms.intValue());
+            //make sound
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // score = length of the throw
+        yourScore.setText("You scored: " + Double.toString(score));
+        checkHighScore();
+        //clear array
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // usen't
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Sensor Manager
+        SensorManager sensorManager;
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer sensor
+        Sensor mySensor;
+        mySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor listener
+        sensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_GAME);
+
+        // Outputs high scores
+        p1 = findViewById(R.id.firstPlace);
+        p2 = findViewById(R.id.secondPlace);
+        p3 = findViewById(R.id.thirdPlace);
+
+        p1.setText(Double.toString(first));
+        p2.setText(Double.toString(second));
+        p3.setText(Double.toString(third));
+
+
+        // initiate button to settings
+        Button settingsButton;
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
+
+    }
+
+    // Assigns the set MIN_ACC chosen in settings
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == RESULT_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                Bundle b = data.getExtras();
+                String temp = b.get(SettingsActivity.SENSITIVITY).toString();
+                MIN_ACC = Integer.parseInt(temp);
+            }
+        }
+    }
+
+    // Opens SettingsActivity
+    public void openSettings() {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivityForResult(i, RESULT_CODE);
+    }
+
+    // checks if the score goes on the scoreboard
+    public void checkHighScore() {
+        if (score >= first) {
+            third = second;
+            second = first;
+            first = score;
+        } else if (score >= second){
+            third = second;
+            second = score;
+        } else if (score >= third) {
+            third = score;
+        }
+
+        // outputs new high scores
+        p1.setText("1. " + Double.toString(first));
+        p2.setText("2. " + Double.toString(second));
+        p3.setText("3. " + Double.toString(third));
+    }
+
+    private static Number calculateACC(float x, float y, float z) {
+        double temp;
+
+        temp = Math.sqrt(x*x + y*y + z*z) - SensorManager.GRAVITY_EARTH;
+
+        return temp;
+    }
+
+
+}
---
+++ app/src/main/java/oddahag/stud/ntnu/no/lab3/SettingsActivity.java
@@ -0,0 +1,89 @@
+package oddahag.stud.ntnu.no.lab3;
+
+import android.app.Activity;
+import android.se.omapi.SEService;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.net.Inet4Address;
+import java.nio.channels.InterruptedByTimeoutException;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static final String SENSITIVITY = "selected_sense";
+    private String sendValue;
+    private static final String S1 = "10";
+    private static final String S2 = "20";
+    private static final String S3 = "30";
+    private static final String S4 = "40";
+    private Button backButton;
+    private SeekBar seekBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Initiate seek bar with a listener
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(3);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // What to do with the different options on the seek bar
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int temp = seekBar.getProgress();
+                switch (temp){
+                    case 0:
+                        sendValue = S1;
+                        break;
+                    case 1:
+                        sendValue = S2;
+                        break;
+                    case 2:
+                        sendValue = S3;
+                        break;
+                    case 3:
+                        sendValue = S4;
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // usen't
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // usen't
+            }
+        });
+
+        // Initiates button to go back to MainActivity
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+    }
+
+    // Function to go back to MainActivity and pass with the chosen sensitivity for the accelerometer
+    public void goBack(){
+        Intent i = new Intent(SettingsActivity.this, MainActivity.class);
+        Bundle b = new Bundle();
+
+        b.putString(SENSITIVITY, sendValue);
+        i.putExtras(b);
+        setResult(Activity.RESULT_OK, i);
+        finish();
+    }
+
+}
